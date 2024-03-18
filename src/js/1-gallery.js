6443645bdcd5ebf473e0d76d72207eb852066235
Leonid Zelov <112327440+LeoniD994@users.noreply.github.com>
2024-03-18 19:40:41 +0200

    fix gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,6 @@
-console.log('gallery');
+// Описаний в документації
+import simpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
