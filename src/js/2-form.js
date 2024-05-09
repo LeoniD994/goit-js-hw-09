80c912e0418fdd3f645aaa80979cad732075ce40
Leonid Zelov <112327440+LeoniD994@users.noreply.github.com>
2024-05-09 14:51:39 +0300

    fix js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,47 @@
-console.log('form');
+const formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+
+window.addEventListener('submit', () => {
+    const storedData = localStorage.getItem('feedback-form-state');
+    if (storedData) {
+    const parsedData = JSON.parse(storedData);
+    emailInput.value = parsedData.email;
+    messageInput.value = parsedData.message;
+    }
+});
+
+
+form.addEventListener('input', (event) => {
+    const { name, value } = event.target;
+    formData[name] = value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+
+    if (formData.email === "" || formData.message === "") {
+    alert('Fill please all fields');
+    return;
+    }
+
+
+    console.log(formData);
+
+
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = "";
+    messageInput.value = "";
+    formData.email = "";
+    formData.message = "";
+});
